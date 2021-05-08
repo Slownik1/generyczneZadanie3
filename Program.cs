8ad81b42ffeb8cae763b9cd3e2875ddc83783fe4
Slownik1 <76266435+Slownik1@users.noreply.github.com>
2021-05-08 15:39:09 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Typygeneryczne_zadanie_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shuffle<int> tabInt = new Shuffle<int>(5);
+
+            for (int i = 0; i < tabInt.tab.Length; i++)
+            {
+                tabInt.tab[i] = i;
+                Console.WriteLine(tabInt.tab[i]);
+            }
+            Console.WriteLine();
+            tabInt.ArrayShuffle(tabInt.tab);
+            
+        }
+    }
+}
---
+++ Shuffle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Typygeneryczne_zadanie_3
+{
+    public class Shuffle<T> :IEnumerable<T>
+    {
+
+        public T[] tab;
+        public int size;
+
+        public Shuffle(int size)
+        {
+            tab = new T[size + 1];
+        }
+
+        public void ArrayShuffle(T[] tab)
+        {
+
+            Random los = new Random();
+
+            for (int i = size-1; i > 0; i--)
+            {
+                int j = los.Next(0, i + 1);
+
+                T temp = tab[i];
+                tab[i] = tab[j];
+                tab[j] = temp;
+                Console.WriteLine(j);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(tab[i]+ " ");
+            }
+
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return ((IEnumerable<T>)tab).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return tab.GetEnumerator();
+        }
+    }
+}
